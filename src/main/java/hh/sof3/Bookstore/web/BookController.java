ada38d99870ec53b3f1f8a9abbc7e88677b737f3
kridesav <savolainenkristjan@gmail.com>
2023-09-19 13:20:20 +0300

    ADDED CRUD

--- src/main/java/hh/sof3/Bookstore/web/BookController.java
+++ src/main/java/hh/sof3/Bookstore/web/BookController.java
@@ -1,31 +1,20 @@
 package hh.sof3.Bookstore.web;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 import hh.sof3.Bookstore.domain.Book;
 import hh.sof3.Bookstore.domain.BookRepository;
 
-// http://localhost:8080/index
+// http://localhost:8080/
 
 @Controller
 public class BookController {
-    private final List<Book> books = new ArrayList<>();
-
-    @RequestMapping(value = "/index", method = RequestMethod.GET)
-    public String index(Model model) {
-        model.addAttribute("books", books);
-        return "index";
-    };
-
     @Autowired
     private BookRepository repository;
 
@@ -33,11 +22,23 @@ public class BookController {
     public String bookList(Model model) {
         model.addAttribute("books", repository.findAll());
         return "booklist";
-    };
+    }
 
-    @PostMapping("/addBook")
-    public String addBook(@RequestParam String title, @RequestParam String author, @RequestParam int year,
-            @RequestParam String isbn, @RequestParam int price) {
-        return "redirect:/index";
-    };
+    @RequestMapping(value = "/addbook")
+    public String addbook(Model model) {
+        model.addAttribute("Book", new Book());
+        return "addbook";
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save (Book book) {
+        repository.save(book);
+        return "redirect:/booklist";
+    }
+    
+    @RequestMapping(value = "/delete/{id}" , method = RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+        repository.deleteById(bookId);
+        return "redirect:/booklist";
+    }
 }
